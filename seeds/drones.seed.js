9b7e1e8ab2c22e11a2ac9f4a7eeb0a8b5de46e90
Alicia Kmak <alicia.kmak@gmail.com>
2021-12-10 08:33:16 +0100

    add iteration 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,33 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to Mongodb");
+  })
+  .then(() => {
+    Drone.insertMany(drones);
+  })
+  .then((insertedDrones) => {
+    console.log(`Inserted ${insertedDrones.length} drones into database`);
+  })
+  .then(() => {
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  });
