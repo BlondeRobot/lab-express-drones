3aed12057bbaad71c111e65590336cbd44fa52b6
Alicia Kmak <alicia.kmak@gmail.com>
2021-12-10 18:24:40 +0100

    add iterations 4 and 5. Done

--- routes/drones.js
+++ routes/drones.js
@@ -23,24 +23,37 @@ router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   const { name, propellers, maxSpeed } = req.body;
   Drone.create({ name, propellers, maxSpeed })
-    //.then(createdDrone => console.log(`New drone created: ${createdDrone.name}))
     .then(() => res.redirect('/drones'))
     .catch((error) => console.log("Error while creating drone occurred"))
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+
+  Drone.findById(id)
+    .then((droneToEdit) => {
+      res.render("/drones/update-form", { droneToEdit })
+    })
+    .catch((error) => next(error));
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+  
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed })
+    .then(() => res.redirect("/drones"))
+    .catch((error) => console.log("Error while updating drone occurred"));
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params;
+  Drone.findByIdAndDelete(id)
+    .then(() => res.redirect("/drones"))
+    .catch((error) => console.log("Error while deleting drone occurred"));
 });
 
 module.exports = router;
